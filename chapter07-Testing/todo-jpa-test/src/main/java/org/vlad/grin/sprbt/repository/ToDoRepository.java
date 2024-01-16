ef8d73fd625bd3099b9e2f17955e62f56178b773
Volodymyr_Hrinchenko <vhrinchenko@dashfinancial.com>
2024-01-16 23:28:32 +0200

    update chapter 7

---
+++ chapter07-Testing/todo-jpa-test/src/main/java/org/vlad/grin/sprbt/repository/ToDoRepository.java
@@ -0,0 +1,11 @@
+package org.vlad.grin.sprbt.repository;
+
+import org.vlad.grin.sprbt.domain.ToDo;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface ToDoRepository extends CrudRepository<ToDo,String> {
+
+    public Iterable<ToDo> findByDescriptionContains(String description);
+
+}
